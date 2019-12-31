11b1bc8e754366f3392ab0435afbb9801c292576
Gaby <Gaby@DESKTOP-LQK39HL>
2019-12-31 07:20:26 -0800

    Add project files.

---
+++ Tema3.1/Userdetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3._1
+{
+    class Userdetails
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
